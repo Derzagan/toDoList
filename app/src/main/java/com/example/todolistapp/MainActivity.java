f152a21c608fc6d8fb719a9b383a81b5e872b117
Isa <Isa@gmail.com>
2025-10-25 17:17:39 +0500

    first

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.todolistapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fabAddTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        fabAddTask = findViewById(R.id.fabAddTask);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        fabAddTask.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, add_task.class);
+            startActivity(intent);
+        });
+
+    }
+}
